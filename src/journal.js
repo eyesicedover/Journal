18534249f212c11975346c573a50e4196c65789c
Maggie Summers and Stephanie Faber <maggie@>
2018-03-12 10:51:54 -0700

    create entry object, create counter and consonant methods

--- src/journal.js
+++ src/journal.js
@@ -1,1 +1,44 @@
-export function Journal()
+export function JournalEntry(title, body) {
+  this.title = title;
+  this.body = body;
+  this.vowelsArray = ["a", "e", "i", "o", "u", "y"];
+  this.specialsArray = [" ", "?", ",", ".", "!", "-"];
+  this.counter = 0;
+  this.vowel = 0;
+  this.consonant = 0;
+  counter();
+}
+
+JournalEntry.prototype.counter = function () {
+  var wordsArray = [];
+  wordsArray = this.body.split(" ");
+  this.counter = wordsArray.length();
+};
+
+JournalEntry.prototype.consonant = function () {
+  var charsArray = [];
+  var tempArray = [];
+  var resultArray = [];
+  charsArray = this.body.split("");
+  for (var i = 0; i < vowelsArray.length(); i++) {
+    for (var j = 0; j < charsArray.length(); j++) {
+      if (charsArray[j] != vowelsArray[i]) {
+        tempArray.push(charsArray[j]);
+      }
+    }
+  }
+  for (var k = 0; k < specialsArray.length(); k++) {
+    for (var l = 0; l < tempArray.length(); l++) {
+      if (tempArray[l] != specialsArray[k]) {
+        resultArray.push(tempArray[l]);
+      }
+    }
+  }
+  this.consonant = resultArray.length();
+};
+
+JournalEntry.prototype.vowel = function () {
+  var charsArray = [];
+  charsArray = this.body.split(" ");
+
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import { Journal } from './journal';
+
+$(document).ready(function() {
+  $('#journal-form').submit(function(event) {
+    event.preventDefault();
+    var title = $('#entry-title').val();
+    var body = $('#entry-body').val();
+    var output = JournalEntry(title, body);
+  });
+});
